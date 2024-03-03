170dde6228d5bfab5f2dc2df19c1cfbadc869864
Konstantin.Mina <k_mina@ukr.net>
2024-03-03 11:58:42 +0200

    Make project a package

--- run.py
+++ run.py
@@ -2,5 +2,8 @@ from app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
+def main():
     app.run(debug=True, host="0.0.0.0", port=8080)
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='BlogCapstone',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+    entry_points={
+        'console_scripts': [
+            'blogcapstone=app.run:main',
+        ],
+    },
+)
