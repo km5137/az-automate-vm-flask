5ed983ad6e2f5f73c031f8fe30687fe4b9877bd2
Konstantin.Mina <k_mina@ukr.net>
2024-03-04 11:25:54 +0200

    Add add azure-keyvault-secrets in requirements

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'flask', 'requests', 'azure-identity'
+        'flask', 'requests', 'azure-identity', 'azure-keyvault-secrets'
     ],
     entry_points={
         'console_scripts': [
